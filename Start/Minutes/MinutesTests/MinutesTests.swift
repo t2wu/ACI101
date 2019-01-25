1d65d03d35f0330c106515325e8f3a6f04f5092c
Timothy Wu <g39328006@ym.edu.tw>
2019-01-25 08:35:11 +0800

    Add test

---
+++ Start/Minutes/MinutesTests/MinutesTests.swift
@@ -0,0 +1,37 @@
+//
+//  MinutesTests.swift
+//  MinutesTests
+//
+//  Created by Timothy Wu on 2019-01-25.
+//  Copyright © 2019 Microsoft. All rights reserved.
+//
+
+import XCTest
+@testable import Minutes
+
+class MinutesTests: XCTestCase {
+    
+    var entryUnderTest: Entry!
+
+    override func setUp() {
+        super.setUp()
+        entryUnderTest = Entry("Title", "Content")
+        
+    }
+
+    override func tearDown() {
+        entryUnderTest = nil
+        super.tearDown()
+    }
+
+    
+    func testSerialization() {
+        let serializedData = FileEntryStore.serialize(entryUnderTest)
+        let entry = FileEntryStore.deserialize(serializedData!)
+        
+        XCTAssertEqual(entry?.title, "Title", "Title does not match")
+        XCTAssertEqual(entry?.content, "Content", "Content does not match")
+    }
+    
+
+}
